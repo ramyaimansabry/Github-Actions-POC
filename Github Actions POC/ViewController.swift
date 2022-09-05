3655478832dfa3c40bf42b7f7300bdd7bbecf3b1
Ramy Sabry <ramysabry1996@gmail.com>
2022-09-05 21:34:52 +0200

    Initial Commit

---
+++ Github Actions POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Actions POC
+//
+//  Created by Ramy Sabry on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
